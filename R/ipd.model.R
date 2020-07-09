e361d0f4b6014de84546c5abb23250b6fbc22249
Michael Seo <swj8874@gmail.com>
2020-07-09 12:06:29 +0200

    revision

--- R/ipd.model.R
+++ R/ipd.model.R
@@ -6,7 +6,7 @@
 #' @param study a vector indicating which study the patient belongs to. Please change the study names into numbers (i.e. 1,2,3,etc)
 #' @param treat a vector indicating which treatment the patient was assigned to. Please change the study names into numbers (i.e. 1,2,3,etc)
 #' @param X a matrix of covariate values for each patient. Dimension would be number of patients x number of covariates.
-#' @param response "continuous" for continuous outcome and "binary" for binary outcome
+#' @param response Specification of the outcomes type. Must specify either "normal" or "binomial"
 #' @param type Assumption on the treatment effect: either "random" for random effects model or "fixed" for fixed effects model. Default is "random".
 #' @param model "onestage" for one-stage model; two-stage are planned to be implemented
 #' @param shrinkage shrinkage method applied to the effect modifiers. "none" correspond to no shrinkage.
@@ -14,14 +14,18 @@
 #' "SSVS" corresponds to the Search Variable Selection method. SSVS is not strictly a shrinkage method, 
 #' but pulls the estimated coefficient toward zero through variable selection in each iteration of the MCMC. 
 #' See O'hara et al (2009) for more reference.
-#' @param mean.alpha Prior mean for the study intercept
-#' @param prec.alpha Prior precision for the study intercept
+#' @param mean.a Prior mean for the study intercept
+#' @param prec.a Prior precision for the study intercept
 #' @param mean.beta Prior mean for the regression coefficients of the main effects of the covariates 
-#' @param prec.beta Prior mean for the regression coefficients of the main effects of the covariates
+#' @param prec.beta Prior precision for the regression coefficients of the main effects of the covariates
+#' @param mean.gamma Prior mean for the effect modifiers. This parameter is not used if penalization is placed on effect modifiers. 
+#' @param prec.gamma Prior precision for the effect modifiers.
 #' @param mean.delta Prior mean for the treatment effect
 #' @param prec.delta Prior precision for the treatment effect
 #' @param hy.prior Prior for the heterogeneity parameter. Supports uniform, gamma, and half normal for normal and binomial response
 #' It should be a list of length 3, where first element should be the distribution (one of dunif, dgamma, dhnorm) and the next two are the parameters associated with the distribution. For example, list("dunif", 0, 5) give uniform prior with lower bound 0 and upper bound 5 for the heterogeneity parameter.
+#' @param lambda.prior (Only for shrinkage = "laplace") Two options for laplace shrinkage. We can put a gamma prior on the lambda (i.e. list("dgamma",2,0.1)) or put a uniform prior on the inverse of lambda (i.e. list("dunif",0,5))
+#' @param p.ind (Only for shrinkage = "SSVS") Prior probability of including each of the effect modifiers. Length should be same as the total length of the covariates.
 #' @return 
 #' \item{data}{Data organized in a list so that it can be used when running code in JAGS}
 #' \item{code}{JAGS code that is used to run the model. Use cat(code) to see the code in a nicer format.}
@@ -29,9 +33,11 @@
 #' @export
 
 ipd.model <- function(y = NULL, study = NULL, treat = NULL, X = NULL, 
-                      response = "continuous", type = "random", model = "onestage", shrinkage = "none"){
+                      response = "normal", type = "random", model = "onestage", shrinkage = "none",
+                      mean.a = 0, prec.a = 0.001, mean.beta = 0, prec.beta = 0.001, mean.gamma = 0, prec.gamma = 0.001,
+                      hy.prior = list("dhnorm", 0, 1), lambda.prior = NULL, p.ind = NULL
+                      ){
 
-  
   data <- 
     list(Nstudies = length(unique(study)),
          Ncovariates = dim(X)[2],
@@ -40,8 +46,13 @@ ipd.model <- function(y = NULL, study = NULL, treat = NULL, X = NULL,
          studyid = study,
          treat = treat,
          y = y)
+  
+  if(is.null(lambda.prior)) lambda.prior <- list("dunif", 0, 5)
+  if(is.null(p.ind)) p.ind <- rep(0.5, dim(X)[2])
          
-  ipd <- list(y = y, study = study, treat = treat, X = X, response = response, type = type, model = model, shrinkage = shrinkage)
+  ipd <- list(y = y, study = study, treat = treat, X = X, response = response, type = type, 
+              model = model, shrinkage = shrinkage, mean.a = mean.a, mean.beta = mean.beta,
+              prec.beta = prec.beta, hy.prior = hy.prior, lambda.prior = lambda.prior)
   
   code <- ipd.rjags(ipd)
 
--- R/ipd.rjags.R
+++ R/ipd.rjags.R
@@ -1,7 +1,6 @@
 
 ipd.rjags <- function(ipd){
   
-  
   type <- ipd$type
   model <- ipd$model
   
@@ -21,11 +20,11 @@ model.onestage <- function(ipd){
     code <- paste0("\n########## IPD-MA model",
                    "\nfor (i in 1:Np) {")
     
-    if(response == "binary"){
+    if(response == "binomial"){
       code <- paste0(code, "\n\ty[i] ~ dbern(p[i])",
                      "\n\tlogit(p[i]) <- a[studyid[i]] + inprod(beta[], X[i,]) +",
                      "\n\t\t(1 - equals(treat[i],1)) * inprod(gamma[], X[i,])")
-    } else if(response == "continuous"){
+    } else if(response == "normal"){
       code <- paste0(code, "\n\ty[i] ~ dnorm(mu[i], sigma)",
                      "\n\tmu[i] <- a[studyid[i]] + inprod(beta[], X[i,]) +",
                      "\n\t\t(1 - equals(treat[i],1)) * inprod(gamma[], X[i,]) +")
@@ -52,12 +51,12 @@ model.onestage <- function(ipd){
     }
     
     if(type == "random"){
-      code <- paste0(code, hy.prior.rjags(hy.prior))  
+      code <- paste0(code, hy.prior.rjags(ipd))  
     }
     
     code <- paste0(code, "\n## prior distribution for baseline effect",
                    "\nfor (j in 1:Nstudies){",
-                   "\n\talpha[j] ~ dnorm(", mean.alpha, ", ", prec.alpha, ")",
+                   "\n\talpha[j] ~ dnorm(", mean.a, ", ", prec.a, ")",
                    "\n}")
     
     code <- paste0(code, "\nfor(k in 1:Ncovariate){",
@@ -65,22 +64,22 @@ model.onestage <- function(ipd){
                    "\n\tbeta[k] ~ dnorm(", mean.beta, ", ", prec.beta, ")",
                    "\n}")
 
-    code <- paste0(code, "## prior distribution for average treatment effect",
-                   "delta[1] <- 0",
-                   "delta[2] ~ dnorm(", mean.delta, ", ", prec.delta, ")")
+    code <- paste0(code, "\n## prior distribution for average treatment effect",
+                   "\ndelta[1] <- 0",
+                   "\ndelta[2] ~ dnorm(", mean.delta, ", ", prec.delta, ")\n")
                    
-
+    code <- paste0(code, shrinkage.prior.rjags(ipd))  
     
-
     return(code)
   })
 }
 
 
 
-hy.prior.rjags <- function(hy.prior){
+hy.prior.rjags <- function(ipd){
   
   code <- ""
+  hy.prior <- ipd$hy.prior
   distr <- hy.prior[[1]]
   if (distr == "dunif") {
     code <- paste0(code,
@@ -95,4 +94,43 @@ hy.prior.rjags <- function(hy.prior){
                    "\n\tsd ~ dnorm(hy.prior.1, hy.prior.2)T(0,)",
                    "\n\ttau <- pow(sd, -2)")
   }
+}
+
+
+shrinkage.prior.rjags <- function(ipd){
+  
+  code <- ""
+  with(ipd, {
+    
+    if(shrinakge == "none"){
+      code <- paste0(code, "\nfor(k in 1:Ncovariate){",
+      "\n\tgamma[k] ~ dnorm(", mean.gamma, ", ", prec.gamma, ") ",
+      "\n}")
+      
+    } else if(shrinkage == "laplace"){
+      
+      if(lambda.prior[[1]] == "dgamma"){
+        if(response == "normal"){
+          code <- paste0(code, "\ntt <- lambda * sigma")
+        } else if(response == "binomial"){
+          code <- paste0(code, "\ntt <- lambda")
+        }  
+        code <- paste0(code, "\nlambda ~ dgamma(", lambda.prior[[2]], ", ", lambda.prior[[3]], ")",
+                       "\nfor(k in 1:Ncovariate){",
+                       "\n\tgamma[k] ~ ddexp(0, tt)",
+                       "\n}")
+                       )
+                       
+      } else if (lambda.prior[[1]] == "dunif"){
+        code <- paste0(code, "\nlambda <- pow(lambda.inv, -1)",
+                       "\nlambda.inv ~ dunif(", lambda.prior[[2]], ", ", lambda.prior[[3]], ")"),
+                       "\nfor(k in 1:Ncovariate){",
+                       "\n\tgamma[k] ~ ddexp(0, lambda)",
+                       "\n}")
+      }
+    }
+    return(code)
+  })
+
+  
 }
