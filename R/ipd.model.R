b7f36589b7730ee15ce059ae2f2032f35207f0e8
Michael Seo <swj8874@gmail.com>
2020-07-09 09:10:43 +0200

    revision

--- R/ipd.model.R
+++ R/ipd.model.R
@@ -7,12 +7,21 @@
 #' @param treat a vector indicating which treatment the patient was assigned to. Please change the study names into numbers (i.e. 1,2,3,etc)
 #' @param X a matrix of covariate values for each patient. Dimension would be number of patients x number of covariates.
 #' @param response "continuous" for continuous outcome and "binary" for binary outcome
+#' @param type Assumption on the treatment effect: either "random" for random effects model or "fixed" for fixed effects model. Default is "random".
 #' @param model "onestage" for one-stage model; two-stage are planned to be implemented
 #' @param shrinkage shrinkage method applied to the effect modifiers. "none" correspond to no shrinkage.
 #' "laplace" corresponds to a adaptive shrinkage with a Laplacian prior (ie often known as Bayesian LASSO).
 #' "SSVS" corresponds to the Search Variable Selection method. SSVS is not strictly a shrinkage method, 
-#' but shrinks the estimated coefficient through variable selection in each iteration of the MCMC. 
+#' but pulls the estimated coefficient toward zero through variable selection in each iteration of the MCMC. 
 #' See O'hara et al (2009) for more reference.
+#' @param mean.alpha Prior mean for the study intercept
+#' @param prec.alpha Prior precision for the study intercept
+#' @param mean.beta Prior mean for the regression coefficients of the main effects of the covariates 
+#' @param prec.beta Prior mean for the regression coefficients of the main effects of the covariates
+#' @param mean.delta Prior mean for the treatment effect
+#' @param prec.delta Prior precision for the treatment effect
+#' @param hy.prior Prior for the heterogeneity parameter. Supports uniform, gamma, and half normal for normal and binomial response
+#' It should be a list of length 3, where first element should be the distribution (one of dunif, dgamma, dhnorm) and the next two are the parameters associated with the distribution. For example, list("dunif", 0, 5) give uniform prior with lower bound 0 and upper bound 5 for the heterogeneity parameter.
 #' @return 
 #' \item{data}{Data organized in a list so that it can be used when running code in JAGS}
 #' \item{code}{JAGS code that is used to run the model. Use cat(code) to see the code in a nicer format.}
@@ -20,7 +29,7 @@
 #' @export
 
 ipd.model <- function(y = NULL, study = NULL, treat = NULL, X = NULL, 
-                               response = "continuous", model = "onestage", shrinkage = "none"){
+                      response = "continuous", type = "random", model = "onestage", shrinkage = "none"){
 
   
   data <- 
@@ -32,10 +41,9 @@ ipd.model <- function(y = NULL, study = NULL, treat = NULL, X = NULL,
          treat = treat,
          y = y)
          
-  ipd <- list(y = y, study = study, treat = treat, X = X, response = response, model = model)
+  ipd <- list(y = y, study = study, treat = treat, X = X, response = response, type = type, model = model, shrinkage = shrinkage)
   
   code <- ipd.rjags(ipd)
-
 
   list(data = data, code = code)
 }
--- R/ipd.rjags.R
+++ R/ipd.rjags.R
@@ -21,8 +21,78 @@ model.onestage <- function(ipd){
     code <- paste0("\n########## IPD-MA model",
                    "\nfor (i in 1:Np) {")
     
-    # code <- paste0 (code, )               
+    if(response == "binary"){
+      code <- paste0(code, "\n\ty[i] ~ dbern(p[i])",
+                     "\n\tlogit(p[i]) <- a[studyid[i]] + inprod(beta[], X[i,]) +",
+                     "\n\t\t(1 - equals(treat[i],1)) * inprod(gamma[], X[i,])")
+    } else if(response == "continuous"){
+      code <- paste0(code, "\n\ty[i] ~ dnorm(mu[i], sigma)",
+                     "\n\tmu[i] <- a[studyid[i]] + inprod(beta[], X[i,]) +",
+                     "\n\t\t(1 - equals(treat[i],1)) * inprod(gamma[], X[i,]) +")
+    }
+    
+    if(type == "random"){
+      code <- paste0(code, " d[studyid[i],treat[i]]",
+                           "\n}")
+    } else if (type == "fixed"){
+      code <- paste0(code, "delta[treat[i]]",
+                           "\n}")
+    }
+    
+    if(response == "normal"){
+      code <- paste0(code, "\nsigma ~ dgamma(0.001, 0.001)")
+    }
+    
+    if(type == "random"){
+      code <- paste0(code, "\n#####treatment effect",
+                     "\nfor(j in 1:Nstudies){",
+                     "\n\td[j,1] <- 0",
+                     "\n\td[j,2] ~ dnorm(delta[2], tau)",
+                     "\n}")
+    }
+    
+    if(type == "random"){
+      code <- paste0(code, hy.prior.rjags(hy.prior))  
+    }
+    
+    code <- paste0(code, "\n## prior distribution for baseline effect",
+                   "\nfor (j in 1:Nstudies){",
+                   "\n\talpha[j] ~ dnorm(", mean.alpha, ", ", prec.alpha, ")",
+                   "\n}")
+    
+    code <- paste0(code, "\nfor(k in 1:Ncovariate){",
+                   "\nfor(k in 1:Ncovariate){",
+                   "\n\tbeta[k] ~ dnorm(", mean.beta, ", ", prec.beta, ")",
+                   "\n}")
+
+    code <- paste0(code, "## prior distribution for average treatment effect",
+                   "delta[1] <- 0",
+                   "delta[2] ~ dnorm(", mean.delta, ", ", prec.delta, ")")
+                   
+
+    
+
     return(code)
   })
 }
 
+
+
+hy.prior.rjags <- function(hy.prior){
+  
+  code <- ""
+  distr <- hy.prior[[1]]
+  if (distr == "dunif") {
+    code <- paste0(code,
+                   "\n\tsd ~ dunif(hy.prior.1, hy.prior.2)",
+                   "\n\ttau <- pow(sd,-2)")
+  } else if(distr == "dgamma"){
+    code <- paste0(code,
+                   "\n\tsd <- pow(prec, -0.5)",
+                   "\n\ttau ~ dgamma(hy.prior.1, hy.prior.2)")
+  } else if(distr == "dhnorm"){
+    code <- paste0(code,
+                   "\n\tsd ~ dnorm(hy.prior.1, hy.prior.2)T(0,)",
+                   "\n\ttau <- pow(sd, -2)")
+  }
+}
