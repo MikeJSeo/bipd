40ddf972b52ecb766a1d2db6da9c4b14b7d53566
Michael Seo <swj8874@gmail.com>
2020-07-11 04:36:50 +0200

    revision

--- R/ipd.run.R
+++ R/ipd.run.R
@@ -6,7 +6,8 @@
 #' @param pars.save parameters to save. "beta" - coefficients for main effects; "gamma" - coefficients for effect modifiers; "delta" - average treaetment effect
 #' @param inits initial values specified for the parameters to save
 #' @param n.chains Number of MCMC chains to 
-#' @param n.adapt The number of iterations for adaptation (this corresponds to the number of burnin)
+#' @param n.adapt The number of iterations for adaptation (Note that the samples from adaptation phase is non-Markovian and do not constitute a Markov chain)
+#' @param n.burnin The number of iterations for burn-in
 #' @param n.iter The number of iterations to run after the adaptation
 #'
 #' @export
@@ -28,7 +29,8 @@ ipd.run <- function(ipd, pars.save = c("beta", "gamma", "delta"), inits = NULL, 
 #' @param pars.save parameters to save. "beta" - coefficients for main effects; "gamma" - coefficients for effect modifiers; "delta" - average treaetment effect
 #' @param inits initial values specified for the parameters to save
 #' @param n.chains Number of MCMC chains to run. This corresponds the number of cores used for parallel computation.
-#' @param n.adapt The number of iterations for adaptation (this corresponds to the number of burnin)
+#' @param n.adapt The number of iterations for adaptation (Note that the samples from adaptation phase is non-Markovian and do not constitute a Markov chain)
+#' @param n.burnin The number of iterations for burn-in
 #' @param n.iter The number of iterations to run after the adaptation
 #'
 #' @export
@@ -37,7 +39,7 @@ ipd.run <- function(ipd, pars.save = c("beta", "gamma", "delta"), inits = NULL, 
 ipd.run.parallel <- function(ipd, pars.save = c("beta", "gamma", "delta"), inits = NULL, n.chains = 3, n.adapt = 1000, n.burnin = 1000, n.iter = 10000){
 
   cl <- parallel::makePSOCKcluster(n.chains)
-  samples <- dclone::jags.parfit(cl = cl, data = ipd$data.JAGS, params = pars.save, model = ipd$model.JAGS, n.adapt = n.adapt, n.iter = n.iter)
+  samples <- dclone::jags.parfit(cl = cl, data = ipd$data.JAGS, params = pars.save, model = ipd$model.JAGS, inits = inits, n.chains = n.chains, n.adapt = n.adapt, n.update = n.burnin, n.iter = n.iter)
   
   return(samples)
 
