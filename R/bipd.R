126c06d7fa42a87c3dd1825e16a47a5a8f6525ca
Michael Seo <swj8874@gmail.com>
2020-07-12 23:27:52 +0200

    revision

--- R/bipd.R
+++ R/bipd.R
@@ -5,7 +5,6 @@
 #'
 #' @docType package
 #' @name bipd-package
-#' @references A.J. Franchini, S. Dias, A.E. Ades, J.P. Jansen, N.J. Welton (2012), \emph{Accounting for correlation in network meta-analysis with multi-arm trials}, Research Synthesis Methods 3(2):142-160. [\url{https://doi.org/10.1002/jrsm.1049}]
 NULL
 
 
