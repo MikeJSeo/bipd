3c9035248a9c273ef1796145a52f4aa8bcffb143
Michael Seo <swj8874@gmail.com>
2021-10-02 14:06:05 +0200

    Update ipdma.impute.R

--- R/ipdma.impute.R
+++ R/ipdma.impute.R
@@ -71,7 +71,7 @@ ipdma.impute <- function(dataset = NULL, covariates = NULL, typeofvar = NULL, in
   imp <- mice(dataset, pred = pred, meth = meth, m = m)
   
   impc <- complete(imp, "long", include = "TRUE")
-  impc.store <- impc[, c(".imp", studyname, outcomename, covariates, grep(treatmentname, colnames(impc), value = TRUE))]
+  impc.store <- impc[, c(".imp", ".id", studyname, outcomename, covariates, grep(treatmentname, colnames(impc), value = TRUE))]
   imp.list <- mitools::imputationList(split(impc.store, impc.store[,1])[-1])$imputations
 
   list(missingPattern = missingPattern, meth = meth, pred = pred, imp = imp, imp.list = imp.list)
