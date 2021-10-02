297cf59654a1d10b8cb5140a07041535ea7eb9e2
Michael Seo <swj8874@gmail.com>
2021-10-02 02:00:15 +0200

    revision

--- R/ipdma.impute.R
+++ R/ipdma.impute.R
@@ -45,8 +45,7 @@ ipdma.impute <- function(dataset = NULL, covariates = NULL, typeofvar = NULL, in
   }
   
   dataset <- dataset %>% select(all_of(c(studyname, treatmentname, outcomename, covariates)))
-  dataset[,covariates[typeofvar == "binary"]] <- lapply(dataset[,covariates[typeofvar == "binary"]], as.factor)
-  
+
   if(interaction == TRUE){
     for(i in 1:length(covariates)){
       varname <- paste0(covariates[i], treatmentname)
@@ -157,8 +156,7 @@ getCorrectMeth <- function(dataset = NULL, missingPattern = NULL, interaction = 
   with(missingPattern, {
     
     dataset <- dataset %>% select(all_of(c(studyname, treatmentname, outcomename, covariates)))
-    dataset[,covariates[typeofvar == "binary"]] <- lapply(dataset[,covariates[typeofvar == "binary"]], as.factor)
-    
+
     if(interaction == TRUE){
       
       trial <- paste0(covariates[1], treatmentname)
@@ -236,8 +234,7 @@ getCorrectPred <- function(dataset = NULL, missingPattern = NULL, interaction = 
   with(missingPattern, {
 
   dataset <- dataset %>% select(all_of(c(studyname, treatmentname, outcomename, covariates)))
-  dataset[,covariates[typeofvar == "binary"]] <- lapply(dataset[,covariates[typeofvar == "binary"]], as.factor)
-  
+
   if(interaction == TRUE){
     
     trial <- paste0(covariates[1], treatmentname)
