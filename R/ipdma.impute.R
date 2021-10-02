b8c7f155844e0e9036ed13b49e1b98d6ce4a2157
Michael Seo <swj8874@gmail.com>
2021-10-02 11:01:29 +0200

    revision

--- R/ipdma.impute.R
+++ R/ipdma.impute.R
@@ -326,20 +326,3 @@ getCorrectPred <- function(dataset = NULL, missingPattern = NULL, interaction = 
   })
 }
 
-
-
-####################### some other helpful functions
-
-
-
-findVarianceUsingRubinsRule <- function(prediction.dummy, variance.dummy){
-  
-  avg.prediction <- apply(prediction.dummy, 1, mean)
-  summ <- 0
-  for(iii in 1:5){
-    summ <- summ + (prediction.dummy[,iii] - avg.prediction)^2
-  }
-  return(apply(variance.dummy, 1, mean) + (5 + 1)/ (5^2 - 5) * summ)
-}
-
-
